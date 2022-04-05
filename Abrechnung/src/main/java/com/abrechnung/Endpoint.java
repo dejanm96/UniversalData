1360cb2eac9275def14ad8550f475e7ee2a20f0d
Dejan Micic <dejo96@gmail.com>
2022-04-05 15:40:19 +0200

    Test

--- Abrechnung/src/main/java/com/abrechnung/Endpoint.java
+++ Abrechnung/src/main/java/com/abrechnung/Endpoint.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
+  
 import com.abrechnung.db.SweetsDB;
 import com.abrechnung.db.Users;
 import com.abrechnung.models.Sweets;
